37f71234d2fbfac07b19018b317ef44bb219de56
Dmitry Bulaev <bulaevd74@mail.ru>
2024-10-22 18:38:44 +0500

    addition image on page

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 fetch("https://dog.ceo/api/breeds/image/random")
   .then((response) => response.json())
-  .then((result) => console.log(result.message));
+  .then((res) => {
+    if (res.status !== "success") {
+      return;
+    }
+    const imgSrc = res.message;
+
+    document.getElementById("imgArea").innerHTML = `<img class="image"
+                                                         src="${imgSrc}" 
+                                                         alt="Картинка с собакой"
+                                                    >`;
+  });
